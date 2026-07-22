a8ab288501f45651f7a2388cb9f1ccea45368128
Furkan Ansari <furkanansari91378@gmail.com>
2026-07-22 11:20:57 +0530

    Refactor array occurrence counting program

--- ARR_DUP.c
+++ ARR_DUP.c
@@ -3,54 +3,36 @@ Q. An array contains 10 elements. Given a number "x" that may occur several time
     of the first occurrence in the array.
 
    soln:
+#include<stdio.h> 
+int main(){ 
+ int n, targ, count=0, occ=-1; 
+ printf("Enter the number of elements in an array: "); 
+ scanf("%d", &n); 
+ 
+ int arr[n]; 
+ for(int i=0;i<n;i++){ 
+ printf("Enter number %d: ", (i+1)); 
+ scanf("%d", &arr[i]); 
+ } 
+ printf("Enter target element: "); 
+ scanf("%d", &targ); 
+ for(int i=0;i<n;i++){ 
+ if(targ == arr[i]){ 
+ count++; 
+ if(occ<0){ 
+ occ = i+1; 
+ } 
+ } 
+ 
+ } 
+ if(count>1){ 
+ printf("The element %d is repeated %d times.\n", targ, count); 
+ printf("The first occurence is at position %d.\n", occ); 
+ } 
+ else{ 
+ printf("Element is not repeated"); 
+ } 
+ return 0; 
+} 
 
-#include <stdio.h>
 
-int main()
-{
-    int n, i, x;
-    
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-
-    printf("Enter the array elements:\n");
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Enter the value of x: ");
-    scanf("%d", &x);
-    int occindex=0;
-    int count = 0;
-    int position = 1;
-
-    for(i = 0; i < n; i++)
-    {
-        if(arr[i] == x)
-        {
-            count++;
-
-            if(occindex == -1)
-            {
-                position = i; 
-               int occindex = i-1;
-            }
-        }
-    }
-
-    if(count > 0)
-    {
-        printf("Occurrence of %d = %d\n", x, count);
-        printf("First occurrence position = %d\n", position);
-        printf("The first index of occurence is:%d",occindex);
-    }
-    else
-    {
-        printf("%d is not present in the array.\n", x);
-    }
-
-    return 0;
-}
